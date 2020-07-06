630a0fa5891ce1a3234f01ca47eeceb8ec20e3e3
Zishan Ansari <zshan.ansari07@gmail.com>
2020-07-06 12:59:27 +0530

    Updated Active Class based on Current URL

--- src/header/Header.js
+++ src/header/Header.js
@@ -6,24 +6,49 @@ import {
 import './Header.css';
 
 class Header extends Component {
+  handleClick = (event) => {
+    const activeURL = document.querySelectorAll('header a');
+    for (let i = 0; i < activeURL.length; i++) {
+      activeURL[i].className = '';
+    }
+    event.target.className = 'active';
+  }
+
   render() {
+
     return (
       <header>
         <nav>
           <ul>
             <li>
-              <Link to="/" title="Home">Home</Link>
+              <Link to="/" title="Home" onClick={this.handleClick}>Home</Link>
             </li>
             <li>
-              <Link to="/about" title="About">About</Link>
+              <Link to="/about" title="About" onClick={this.handleClick}>About</Link>
             </li>
             <li>
-              <Link to="/users" title="Users">Users</Link>
+              <Link to="/users" title="Users" onClick={this.handleClick}>Users</Link>
             </li>
           </ul>
         </nav>
       </header>
     );
+  }
+}
+
+window.onload = () => {
+  const activeURL = document.querySelectorAll('header a');
+  for (let i = 0; i < activeURL.length; i++) {
+    activeURL[i].className = '';
+  }
+  if (window.location.pathname === '/') {
+    activeURL[0].className = 'active';
+  }
+  if (window.location.pathname === '/about') {
+    activeURL[1].className = 'active';
+  }
+  if (window.location.pathname === '/users') {
+    activeURL[2].className = 'active';
   }
 }
 
